2b9ccfe1c34f7033704a07223392b46f13c7390c
anandabenediktsdottir <ananda@live.no>
2025-03-31 13:30:39 +0200

    changed to minimal theme

--- penguins_plot.R
+++ penguins_plot.R
@@ -6,5 +6,5 @@ library(tidyverse)
 ggplot(penguins, aes(x=bill_depth_mm, y = bill_length_mm, color=species))+
   labs(x = "Bill depth (mm)", y = "Bill length (mm)") +
   geom_point()+
-  theme_bw()
+  theme_minimal()
   
