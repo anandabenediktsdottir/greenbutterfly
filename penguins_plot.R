2695fcee9a4e5dcd9dcf1a68d41cbeaebd036215
anandabenediktsdottir <ananda@live.no>
2025-03-31 11:12:42 +0200

    added axes titlea

--- penguins_plot.R
+++ penguins_plot.R
@@ -4,5 +4,6 @@ library(tidyverse)
 
 
 ggplot(penguins, aes(x=bill_depth_mm, y = bill_length_mm, color=species))+
+  labs(x = "Bill depth (mm)", y = "Bill length (mm)")
   geom_point()+
   theme_minimal()
