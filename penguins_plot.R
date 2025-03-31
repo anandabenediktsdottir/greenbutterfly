39a24ee6fc0d5fe9f3d210ec1718b9b011a8d2f5
anandabenediktsdottir <ananda@live.no>
2025-03-31 13:31:27 +0200

    Update penguins_plot.R
    removed theme

--- penguins_plot.R
+++ penguins_plot.R
@@ -5,6 +5,5 @@ library(tidyverse)
 
 ggplot(penguins, aes(x=bill_depth_mm, y = bill_length_mm, color=species))+
   labs(x = "Bill depth (mm)", y = "Bill length (mm)") +
-  geom_point()+
-  theme_bw()
+  geom_point()
   
